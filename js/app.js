3a8f5c251f5d50047807ef9a3013c04463187905
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-11-14 19:34:17 +0200

    add all freds

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// gsap.to(".fred", { x: 400, y: 200, scale: 3, rotation: 360, duration: 3 });
+
+// gsap.fromTo(".fred", { x: 700, y: 400, opacity: 0 }, { x: 400, y: 200, scale: 3, duration: 3, opacity: 1 });
+
+// gsap.to(".green", { x: 600, duration: 2, repeat: 1, yoyo: true });
+
+// gsap.to(".green", { x: 600, duration: 2, easy: "bounce.inOut" });
+
+//elastic, inOut (ANIM)
+
+// gsap.to(".green", { x: 600, duration: 2, easy: "bCK" });
+
+// gsap.to(".pink", { x: 600, duration: 2, easy: "linear" });
+
