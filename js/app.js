e5533386adc1223574b4ed3fc44233a37d101ed5
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-11-19 12:12:12 +0200

    add default parameters

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-const tl = gsap.timeline();
-tl.from("#demo", { opacity: 0 })
-  .from(".title", { x: 80, opacity: 0 })
-  .from(".subtitle", { x: -80, opacity: 0 })
-  .from(".text", { y: 30, opacity: 0 })
-  .from(".myButton", { y: 30, opacity: 0 })
-  .from("#items > g", { opacity: 0, scale: 0, stagger: 0.3, transformOrigin: "50% 50%" })
+const tl = gsap.timeline({ defaults: { opacity: 0, easy: "back" } });
+tl.from("#demo", { easy: "linear" })
+  .from(".title", { x: 80 })
+  .from(".subtitle", { x: -80 })
+  .from(".text", { y: 30 })
+  .from(".myButton", { y: 30 })
+  .from("#items > g", { scale: 0, stagger: 0.3, transformOrigin: "50% 50%" })
