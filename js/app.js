3bd257f58596de03f9d716fd477b3ef20eefaa26
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-11-20 12:09:19 +0200

    add some styles

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-const tl = gsap.timeline({ defaults: { opacity: 0, easy: "back" } });
-tl.from("#demo", { easy: "linear" })
-  .from(".title", { x: 80 })
-  .from(".subtitle", { x: -80 })
-  .from(".text", { y: 30 })
-  .from(".myButton", { y: 30 })
-  .from("#items > g", { scale: 0, stagger: 0.3, transformOrigin: "50% 50%" })
+const tl = gsap.timeline({ defaults: { autoAlpha: 0, easy: "back" } });
+
+function init() {
+  tl.from("#demo", { easy: "linear" })
+    .from(".title", { x: 80 })
+    .from(".subtitle", { x: -80 }, "<")
+    .from(".text", { y: 30 }, "-=0.2")
+    .from(".myButton", { y: 50 }, "-=0.4")
+    .from("#items > g", { scale: 0, stagger: 0.3, transformOrigin: "50% 50%" }, "-=0.5");
+}
+
+window.addEventListener("load", function (event) {
+  init();
+});
