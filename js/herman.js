85ec82d045c6607051b9e0b67dfee7bee04508fd
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-22 15:57:32 +0200

    done

--- js/herman.js
+++ js/herman.js
@@ -1,8 +1,18 @@
 const hole = document.querySelector(".hole");
 const herman = document.querySelector(".herman");
+const shadow = document.querySelector(".shadow");
+const tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 0.5 });
 
-const tl = gsap
-  .timeline()
-  .from(hole, { scale: 0, repeat: 1, yoyo: true })
-  .fromTo(herman, { y: 110 }, { y: -175 }, 0.2)
-  .to(herman, { y: 0, easy: "power1.in" }, ">");
+tl.from(hole, { scale: 0, repeat: 1, yoyo: true })
+  .fromTo(herman, { y: 160, scaleY: 2 }, { y: -175, scaleY: 1 }, 0.2)
+  .to(herman, { y: -4, easy: "power1.in" }, ">")
+  .to(herman, {
+    scaleY: 0.9,
+    scaleX: 1.3,
+    transformOrigin: "50% 100%",
+    repeat: 1,
+    yoyo: true,
+    duration: 0.2,
+  })
+  .to(shadow, { opacity: 1, duration: 0.2 }, 0.7)
+  .to(shadow, { scaleX: 0.5, easy: "power1.in" }, ">");
