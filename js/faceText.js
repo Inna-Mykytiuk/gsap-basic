337d7796a3dc47a1462601eb7c29c035a227cb2f
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-11-25 11:59:26 +0200

    face text

---
+++ js/faceText.js
@@ -0,0 +1,24 @@
+var tl = gsap.timeline({ repeat: 5 });
+var duration = 2;
+function init() {
+  tl.fromTo(
+    "h1",
+    { scale: 0 },
+    { duration: duration, scale: 6, ease: "slow(0.5, 0.8)", stagger: duration }
+  ).from(
+    "h1",
+    {
+      duration: duration,
+      opacity: 0,
+      ease: "slow(0.5, 0.8, true)",
+      stagger: duration,
+    },
+    "<"
+  );
+}
+
+window.addEventListener("load", function (event) {
+  // console.log("load");
+  gsap.set("#memo", { autoAlpha: 1 });
+  init();
+});
